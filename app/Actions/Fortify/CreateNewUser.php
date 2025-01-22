9eab1d3d776d5c4ccfc6d3253b0d680d341c40bb
Gaurang Parekh <gaurang9rising@gmail.com>
2025-01-22 17:56:17 +0000

    Laravel Pint

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -17,7 +17,8 @@ class CreateNewUser implements CreatesNewUsers
      *
      * @param  array<string, string>  $input
      */
-    public function create(array $input): User {   
+    public function create(array $input): User
+    {
 
         // this is  a arndom comment 5 - my good pr 5
 
