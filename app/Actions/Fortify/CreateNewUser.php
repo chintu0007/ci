c2a52a96e804a4f857b3e41089893e21a6447c3f
Gaurang Parekh <gaurang9rising@gmail.com>
2025-01-22 20:33:46 +0530

    random comment

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -20,7 +20,7 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {   
 
-        // this is  a arndom number
+        // this is  a arndom comment
 
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
