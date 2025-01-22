eddf82b074a51d9d1a0df4f6ba07aafca4c0833d
Gaurang Parekh <gaurang9rising@gmail.com>
2025-01-22 21:20:39 +0530

    my-good-pr 5

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -20,7 +20,7 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {   
 
-        // this is  a arndom comment 5 - my good pr 
+        // this is  a arndom comment 5 - my good pr 5
 
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
